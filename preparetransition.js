2d9e0215db578c749013bdd3219d641ccaa26b34
Jonathan Snook <jonathan.snook@jadedpixel.com>
2012-01-12 11:06:46 -0500

    initial plugin

---
+++ preparetransition.js
@@ -0,0 +1,44 @@
+/**
+ *  prepareTransition
+ *  jQuery Plugin for ensuring transitions with display:none or visibility:hidden 
+ *  are in the right state until the end of the transition
+ *
+ *  Developed by Jonathan Snook (http://snook.ca/)
+ *  January 12, 2012
+ *
+ *  Requires the following CSS:
+ *  .is-transitioning {
+ *      display: block !important;
+ *      visibility: hidden !important;
+ *  }
+ */
+
+(function($){
+
+$.fn.prepareTransition = function(){
+    return this.each(function(){
+        var el = $(this);
+        // remove the transition class upon completion
+        el.one('TransitionEnd webkitTransitionEnd transitionend oTransitionEnd', function(){
+            console.log('here');
+            el.removeClass('is-transitioning');
+        });
+
+        // check the various CSS properties to see if a duration has been set
+        var cl = ["transition-duration", "-moz-transition-duration", "-webkit-transition-duration", "-o-transition-duration"];
+        var duration = 0;
+        $.each(cl, function(idx, itm){
+            duration =  parseFloat( el.css( itm ) ) || duration;
+        });
+
+        // if I have a duration then add the class
+        if (duration != 0) {
+            el.addClass('is-transitioning');
+            el[0].offsetWidth; // check offsetWidth to force the style rendering 
+        };
+    });
+}
+
+
+}(jQuery));
+
