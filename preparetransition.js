887e1ca91fa85ca1feb04cfeca831a124c3682d7
Oliver Joseph Ash <oliverash@me.com>
2012-02-04 11:22:26 +0000

    Improve code for efficiency and size (down from 531 bytes to 434 bytes)

--- preparetransition.js
+++ preparetransition.js
@@ -1,46 +1,45 @@
 /**
- *  prepareTransition
- *  jQuery Plugin for ensuring transitions with display:none or visibility:hidden 
- *  are in the right state until the end of the transition
+ * prepareTransition
+ * jQuery Plugin for ensuring transitions with display: none or visibility: hidden are in the right state until the end of the transition
  *
- *  Developed by Jonathan Snook (http://snook.ca/)
- *  January 12, 2012
+ * Developed by Jonathan Snook (http://snook.ca/)
+ * January 12, 2012
  *
- *  Requires the following CSS:
- *  .is-transitioning {
- *      display: block !important;
- *      visibility: visible !important;
- *  }
+ * Improved by Oliver Ash (http://twitter.com/OliverJAsh)
+ * February 4, 2012
  *
- *  MIT license 
- *  http://www.opensource.org/licenses/mit-license.php
+ * Requires the following CSS:
+ * .is-transitioning {
+ *  	display: block !important;
+ *  	visibility: visible !important;
+ * }
+ *
+ * MIT license 
+ * http://www.opensource.org/licenses/mit-license.php
  */
 
-(function($){
+;(function ($) {
+	$.fn.prepareTransition = function () {
+		var durationVendorPrefixes = ["transition-duration", "-moz-transition-duration", "-webkit-transition-duration", "-o-transition-duration"];
 
-$.fn.prepareTransition = function(){
-    return this.each(function(){
-        var el = $(this);
-        // remove the transition class upon completion
-        el.one('TransitionEnd webkitTransitionEnd transitionend oTransitionEnd', function(){
-            el.removeClass('is-transitioning');
-        });
+		return this.each(function () {
+			var $this = $(this),
+				duration = 0;
 
-        // check the various CSS properties to see if a duration has been set
-        var cl = ["transition-duration", "-moz-transition-duration", "-webkit-transition-duration", "-o-transition-duration"];
-        var duration = 0;
-        $.each(cl, function(idx, itm){
-            duration =  parseFloat( el.css( itm ) ) || duration;
-        });
+			// Check the various CSS properties to see if a duration has been set
+			$.each(durationVendorPrefixes, function (index, prefix) {
+				duration = parseFloat($this.css(prefix)) || duration;
+			});
 
-        // if I have a duration then add the class
-        if (duration != 0) {
-            el.addClass('is-transitioning');
-            el[0].offsetWidth; // check offsetWidth to force the style rendering 
-        };
-    });
-}
-
-
+			// If I have a duration then prepare transition
+			if (duration !== 0) {
+				$this
+					.addClass('is-transitioning')
+					.one('TransitionEnd webkitTransitionEnd transitionend oTransitionEnd', function () {
+						$this.removeClass('is-transitioning');
+					})
+					.width(); // Force the style rendering
+			}
+		});
+	};
 }(jQuery));
-
