afdb199afc4538e9817053075288b86dd26ffbaa
Oliver Joseph Ash <oliverash@me.com>
2012-06-20 21:11:40 +0100

    Clean up example, update jQuery version

--- jquery.preparetransition.js
+++ jquery.preparetransition.js
@@ -1,5 +1,5 @@
 /**
- * prepareTransition
+ * PrepareTransition
  * jQuery Plugin for ensuring transitions with display: none or visibility: hidden are in the right state until the end of the transition
  *
  * Developed by Jonathan Snook (http://snook.ca/)
@@ -10,36 +10,42 @@
  *
  * Requires the following CSS:
  * .is-transitioning {
- *  	display: block !important;
- *  	visibility: visible !important;
+ *    display: block !important;
+ *    visibility: visible !important;
  * }
  *
- * MIT license 
+ * MIT license
  * http://www.opensource.org/licenses/mit-license.php
  */
 
 ;(function ($) {
-	$.fn.prepareTransition = function () {
-		var durationVendorPrefixes = ["transition-duration", "-moz-transition-duration", "-webkit-transition-duration", "-o-transition-duration"];
 
-		return this.each(function () {
-			var $this = $(this),
-				duration = 0;
+  $.fn.prepareTransition = function () {
 
-			// Check the various CSS properties to see if a duration has been set
-			$.each(durationVendorPrefixes, function (index, prefix) {
-				duration = parseFloat($this.css(prefix)) || duration;
-			});
+    var durationVendorPrefixes = ["transition-duration", "-moz-transition-duration", "-webkit-transition-duration", "-o-transition-duration"];
 
-			// If I have a duration then prepare transition
-			if (duration !== 0) {
-				$this
-					.addClass('transitioning')
-					.one('TransitionEnd webkitTransitionEnd transitionend oTransitionEnd', function () {
-						$this.removeClass('transitioning');
-					})
-					.width(); // Force the style rendering
-			}
-		});
-	};
+    return this.each(function () {
+
+      var $this = $(this),
+        duration = 0;
+
+      // Check the various CSS properties to see if a duration has been set
+      $.each(durationVendorPrefixes, function (index, prefix) {
+        duration = parseFloat($this.css(prefix)) || duration;
+      });
+
+      // If I have a duration then prepare transition
+      if (duration !== 0) {
+        $this
+          .addClass('is-transitioning')
+          .one('TransitionEnd webkitTransitionEnd transitionend oTransitionEnd', function () {
+            $this.removeClass('is-transitioning');
+          })
+          .width(); // Force the style rendering
+      }
+
+    });
+
+  };
+
 }(jQuery));
