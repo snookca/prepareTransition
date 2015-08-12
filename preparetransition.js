c6d07b665855663b11e75daeb4dc9ed4d3f130d4
Jim Doyle <jimdoyle82@gmail.com>
2015-08-12 10:45:15 +1000

    Got basic version of loader working for ie9 (unstyled)

--- preparetransition.js
+++ preparetransition.js
@@ -18,18 +18,42 @@
 
 (function($){
 
+    function supportsTransitions() {
+        var b = document.body || document.documentElement,
+            s = b.style,
+            p = 'transition';
+
+        if (typeof s[p] == 'string') { return true; }
+
+        // Tests for vendor specific prop
+        var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
+        p = p.charAt(0).toUpperCase() + p.substr(1);
+
+        for (var i=0; i<v.length; i++) {
+            if (typeof s[v[i] + p] == 'string') { return true; }
+        }
+
+        return false;
+    }
+
 /**
  * @param parentEl {jQuery Element} optional - If passed in the 'is-transitioning' class will be added/removed from here rather than the "this" element
  */
 $.fn.prepareTransition = function( parentEl ){
+    var hasTrans = supportsTransitions();
+
     return this.each(function(){
         var el = $(this);
         if( !parentEl ) parentEl = el;
         
-        // remove the transition class upon completion
-        el.one('TransitionEnd webkitTransitionEnd transitionend oTransitionEnd', function(evt){
-            parentEl.removeClass('is-transitioning');
-        });
+        if( hasTrans ) {
+            // remove the transition class upon completion
+            el.one('TransitionEnd webkitTransitionEnd transitionend oTransitionEnd', function(evt){
+                parentEl.removeClass('is-transitioning');
+            });
+        } else {
+            el.addClass('hasnt-transition');
+        }
 
         // check the various CSS properties to see if a duration has been set
         var cl = ["transition-duration", "-moz-transition-duration", "-webkit-transition-duration", "-o-transition-duration"];
@@ -40,7 +64,7 @@ $.fn.prepareTransition = function( parentEl ){
 
         // if I have a duration then add the class
         if (duration != 0) {
-            parentEl.addClass('is-transitioning');
+            if( hasTrans ) parentEl.addClass('is-transitioning');
             el[0].offsetWidth; // check offsetWidth to force the style rendering
         };
     });
